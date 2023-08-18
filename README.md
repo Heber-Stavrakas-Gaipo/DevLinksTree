e56fb7b5072ed5908d0fe41670361acc27858612
Héber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2023-08-17 23:10:52 -0300

    Final class commit

--- README.md
+++ README.md
@@ -49,4 +49,7 @@ Esse projeto está sob a licença MIT.
 
 ---
 
+### OBS:
+O estágio atual do projeto exemplifica a plena execução do que foi visto nas aulas do curso. Em breve pretendo enriquecê-lo mais e adicionar minha identidade visual e assinatura.
+
 Feito com ♥ by Rocketseat :wave: [Participe da nossa comunidade!](https://discord.gg/rocketseat)
--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function toggleMode() {
 
   const img = html.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "https://github.com/heber-stavrakas-gaipo.png") //Ainda será adicionada uma foto para tema claro
     img.setAttribute("alt", "Imagem de perfil para o tema claro")
   } else {
-    img.setAttribute("src", "https://github.com/heber-stavrakas-gaipo.png")
+    img.setAttribute("src", "https://github.com/heber-stavrakas-gaipo.png") // E outra para escuro
     img.setAttribute("alt", "Imagem de perfil para o tema escuro")
   }
 }
