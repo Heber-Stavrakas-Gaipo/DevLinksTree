076e4680012c1e60290db400812a264a4a6e1082
Heber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2024-05-01 20:51:20 -0300

    :lipstick: add dark mode and light mode Octocat stick in footer

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = html.querySelector("#profile img")
+  const octocat = html.querySelector("#octocat")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "https://github.com/heber-stavrakas-gaipo.png")
     img.setAttribute("alt", "Imagem de perfil para o tema claro")
+    octocat.setAttribute("src", "./assets/octocat-lightMode.png")
   } else {
     img.setAttribute("src", "https://github.com/heber-stavrakas-gaipo.png")
     img.setAttribute("alt", "Imagem de perfil para o tema escuro")
+    octocat.setAttribute("src", "./assets/octocat-darkMode.png")
   }
 }
